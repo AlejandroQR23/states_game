e6b497492c4d3954e1f61339ded663708e744e5a
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2021-05-18 01:51:13 -0500

    creates a csv with the missing states

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import pandas
 def get_mouse_coordinates(x, y):
     print( x, y )
 
+def get_missing_states( states, guessed_states ):
+    missing_states = list( set(states) - set(guessed_states) )  
+    new_data = pandas.DataFrame( missing_states )
+    new_data.to_csv( "states_to_learn.csv" )
+
 screen = turtle.Screen()
 screen.title( "U.S. States Game" )
 
@@ -26,9 +31,15 @@ guessed_states = []
 while len(guessed_states) < 50:
 
     guessed = len(guessed_states)
-    
+
+
     # * Get the user answer
     input_state = screen.textinput( title=f"{guessed}/50 Guess the state", prompt="What's another state's name?" ).title()
+
+    # * Exit the screen
+    if input_state == "Exit":
+        get_missing_states( states, guessed_states )
+        break
 
     # * Check the user answer
     if input_state in states:
@@ -41,5 +52,3 @@ while len(guessed_states) < 50:
         state_data = data[ data.state == input_state ]
         t.goto( int(state_data.x), int(state_data.y) )
         t.write( input_state )
-
-turtle.mainloop()
